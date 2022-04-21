b13b93c7db7a97131799a7f81fa563733d6570af
Ajian8023 <43978465+Ajian8023@users.noreply.github.com>
2022-04-21 19:04:26 +0800

    Update json_utils.dart

--- lib/json/json_utils.dart
+++ lib/json/json_utils.dart
@@ -64,7 +64,7 @@ class JsonUtils {
     httpLogMap.putIfAbsent("请求头", () => response.requestOptions.headers);
     httpLogMap.putIfAbsent(
         "query参数", () => response.requestOptions.queryParameters);
-    httpLogMap.putIfAbsent('body参数',()=>response.data);
+    httpLogMap.putIfAbsent('body参数',()=>response.requestOptions.data);
     httpLogMap.putIfAbsent("响应数据", () => response.data);
 
 
